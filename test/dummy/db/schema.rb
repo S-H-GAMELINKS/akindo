0206cc6818a17e22bb80d54e1fc303c6d5a5a287
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-02-02 16:48:52 +0900

    db migrate #3

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_143525) do
+ActiveRecord::Schema.define(version: 2020_02_02_074405) do
 
   create_table "akindo_categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "akindo_products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
